1aaeed7de2a405e0f05c718005cdc7534b90a6a2
mmeany <mark.meany@os.uk>
2017-01-19 14:30:51 +0000

    Initial

---
+++ src/main/java/uk/os/mvm/play/MvnPlayApplication.java
@@ -0,0 +1,12 @@
+package uk.os.mvm.play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnPlayApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/os/mvm/play/MvnPlayApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.os.mvm.play;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MvnPlayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
